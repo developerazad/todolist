b544bda66c371f7ad024e6cea66cae9bd1a2da56
developerazad <developerazad.hstu@gmail.com>
2019-06-18 01:10:19 +0600

    making index with todo list

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Todo List</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <div id="footer">
+        <p class="text-center">Copy &copy; 2019, Developerazad</p>
+    </div>
+</body>
+</html>
